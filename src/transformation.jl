fb350d050265b32afca403294ced5f5f7e8e4cd1
Tom McGrath <tmcgrath325@gmail.com>
2026-07-08 10:19:22 -0500

    SDFAtom coords as Vector

--- src/transformation.jl
+++ src/transformation.jl
@@ -15,7 +15,7 @@ preserved.
 """
 function transformed(tform, a::SDFAtom)
     newcoords = tform(a.coords)
-    return SDFAtom(a.symbol, a.charge, a.multiplicity, getmass(a), newcoords)
+    return SDFAtom(a.symbol, a.charge, a.multiplicity, getmass(a), Vector{eltype(newcoords)}(newcoords))
 end
 
 function transformed(tform, mol::SDFMolGraph)
